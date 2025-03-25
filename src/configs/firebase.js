4050c574be19fe05e8560adb959554a4e39e5c47
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-03-25 12:02:44 +0100

    first commit

---
+++ src/configs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5SvrOOBSXt_TO5CmP_7QdH26d8xeySz4",
+  authDomain: "vault-opgave.firebaseapp.com",
+  projectId: "vault-opgave",
+  storageBucket: "vault-opgave.firebasestorage.app",
+  messagingSenderId: "859392652966",
+  appId: "1:859392652966:web:ebb1f6296bb01caae06667"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
