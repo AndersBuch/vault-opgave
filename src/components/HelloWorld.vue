771d2b041d121a568d3ba04d3dfc5fcfc6dcb841
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-03-31 11:54:13 +0200

    asdas
    dasd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,70 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
+import { auth } from '@/configs/firebase.js'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+import { ref } from 'vue';
+
+const data = ref({
+  email: '',
+  password: ''
 })
+
+const mode = ref('login');
+
+const user = ref(null);
+
+function toggleMode(val) {
+  mode.value = val
+}
+
+async function login(email, password) {
+  await signInWithEmailAndPassword(auth, email, password).then((res) => {
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+async function register(email, password) {
+  await createUserWithEmailAndPassword(auth, email, password).then((res) => {
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+function submit() {
+let email = data.value.email
+let password = data.value.password
+if(mode.value === 'login') {
+  login(email, password)
+}
+else {
+  register(email, password)
+}
+}
+
+async function signout() {
+  await signOut(auth).then((res) => {}).catch((err) => {
+    console.log(err);
+  })
+}
+
+onAuthStateChanged(auth, currentUser => {
+user.value = currentUser;
+})
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div>
+    <div v-if="user">{{ user?.email }}<button @click="signout">Sign out</button></div>
+    <form @submit.prevent="submit">
+      <div><input type="email" placeholder="Enter email" v-model="data.email"></div>
+      <div><input type="password" placeholder="Enter password" v-model="data.password"></div>
+      <button type="submit">{{ mode === 'login' ? 'Login' : 'Register'}}</button>
+      <div @click="toggleMode(mode === 'login' ? 'register': 'login')">{{ mode === 'login' ? 'Not a user? Register': 'Already a user? Login'}}</div>
+    </form>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
